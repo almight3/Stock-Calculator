a2b0b1870b2f6a8fc42d78ec0247fed1d9bee88b
almight3 <shrivastavap136@gmail.com>
2021-08-22 11:37:26 +0530

    features

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ let currentPrice = document.querySelector("#current-price")
 let stocksQuantity = document.querySelector("#quantity-of-stocks")
 const outputMessage = document.querySelector("#output-message")
 let checkButton = document.querySelector("#check-button")
+const changeBackground = document.querySelector("#backgroundcolor-change")
+const stockGif = document.querySelector("#stocks-gif")
 
 checkButton.addEventListener("click", checkProfitLoss);
 function checkProfitLoss(){
@@ -16,18 +18,20 @@ function checkProfitLoss(){
 }
 
 function calculateProfitLoss(intial,quantity,current){
+    let flage = false;
     if(intial > current){
         let loss = (intial - current) * quantity;
         let lossPercentage = (loss / intial) * 100;
-        let flage = true;
+        flage = true;
         showMessage(`Hey, the loss is ${loss} and the percent is ${lossPercentage}%`)
-         
+        changeBackgroundColor(flage); 
 
     }
     else if( current > intial){
         let profit =  (current - intial) * quantity;
         let profitPercentage = (profit / current) * 100;
-        let flage = false;
+        //let flage = false;
+        changeBackgroundColor(flage);
         showMessage(`Hey, the profit is ${profit} and the percent is ${profitPercentage}%`)
     }
 
@@ -37,9 +41,19 @@ function calculateProfitLoss(intial,quantity,current){
              
 }
 
-function showMessage(msg, flage){
+function showMessage(msg){
   outputMessage.style.display = "block";
   
   outputMessage.innerText = msg;
   
 }
+
+function changeBackgroundColor(flage){
+    if(flage == true)
+    changeBackground.style.backgroundColor = "red";
+    else
+    changeBackground.style.backgroundColor = "#BEF264";
+
+  console.log("check")
+}
+
